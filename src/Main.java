f0f95519062e52212a12eca1658c45059a5d8fc2
HelloWorld <sapanovs04@gmail.com>
2025-07-30 12:02:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        String ageTrue = age >= 19 ? "Можно выпить!" : "Нельзя пить.";
+        out.println(ageTrue);
+
+    }
+}
