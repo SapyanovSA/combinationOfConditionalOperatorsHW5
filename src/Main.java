d40521123ba1faa86616854283c4305cc36ef1ad
HelloWorld <sapanovs04@gmail.com>
2025-07-31 11:12:06 +0300

    Remade homework 5

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         // Задача 1
-        int clientOS = 2;
+        int clientOS = 1;
         System.out.println("Задача 1");
         if (clientOS == 0) {
-            System.out.println("Ответ: Установите версию приложения для iOS по ссылке.");
+            System.out.println("Установите версию приложения для iOS по ссылке.");
         } else if (clientOS == 1) {
-            System.out.println("Ответ: Установите версию приложения для Android по ссылке.");
-        } else {
-            System.out.println("Такого варианта нет.");
+            System.out.println("Установите версию приложения для Android по ссылке.");
         }
         System.out.println();
 
@@ -18,35 +16,26 @@ public class Main {
         int clientIOS = 1;
         int clientDeviceYear = 2015;
         System.out.println("Задача 2");
-        if (clientIOS == 0) {
-            System.out.println("Ответ: Установите версию приложения для iOS по ссылке.");
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            }
-        } else if (clientIOS == 1) {
-            System.out.println("Ответ: Установите версию приложения для Android по ссылке.");
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            }
-        } else {
-            System.out.println("Такого варианта нет.");
+        if (clientIOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientIOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientIOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        } else if (clientIOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
         System.out.println();
 
         //Задача 3
         int year = 2021;
-        boolean leapYear = (year - 1584) % 400 == 0;
-        System.out.println("Задача 3");
-        if (leapYear) {
-            System.out.println("Ответ: " + year + " год является високосным.");
-        } else if (year > 1584 && year % 4 == 0) {
-            if (year % 100 == 0) {
-                System.out.println("Ответ: " + year + " год не является високосным.");
-            } else {
-                System.out.println("Ответ: " + year + " год является високосным.");
-            }
+        System.out.println("Задание 3");
+        if (year > 1584 && (year % 4 != 0 || year % 100 == 0 && year % 400 != 0)) {
+            System.out.println(year + " год не является високосным.");
+        } else if (year >= 1584 & (year % 4 == 0 || year % 400 == 0)) {
+            System.out.println(year + " год является високосным.");
         } else {
-            System.out.println("Ответ: " + year + " год не является високосным.");
+            System.out.println("Весокосный год был введен в 1584 году.");
         }
         System.out.println();
 
