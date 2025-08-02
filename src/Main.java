955da4fa0c5087221ed22dafd17a0226e0b66bf5
HelloWorld <sapanovs04@gmail.com>
2025-08-02 12:42:08 +0300

    Remade homework 5

--- src/Main.java
+++ src/Main.java
@@ -57,27 +57,19 @@ public class Main {
         System.out.println();
 
         // Задача 5
-        int monthNumber = 12;
+        int monthNumber = 7;
         System.out.println("Задание 5");
         switch (monthNumber) {
-            case 1:
-            case 2:
-            case 12:
+            case 1, 2, 12:
                 System.out.println("Ответ: " + monthNumber + "-й месяц относится к сезону зима.");
                 break;
-            case 3:
-            case 4:
-            case 5:
+            case 3, 4, 5:
                 System.out.println("Ответ: " + monthNumber + "-й месяц относится к сезону весна.");
                 break;
-            case 6:
-            case 7:
-            case 8:
+            case 6, 7, 8:
                 System.out.println("Ответ: " + monthNumber + "-й месяц относится к сезону лето.");
                 break;
-            case 9:
-            case 10:
-            case 11:
+            case 9, 10, 11:
                 System.out.println("Ответ: " + monthNumber + "-й месяц относится к сезону осень.");
                 break;
             default:
